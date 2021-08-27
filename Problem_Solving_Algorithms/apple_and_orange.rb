17217ef65f6cef85125d287baf40dbbe07241d9a
furtadomn <mnfurtado@yahoo.com.br>
2021-08-27 11:56:57 -0300

    apple_and_orange

--- Problem_Solving_Algorithms/apple_and_orange.rb
+++ Problem_Solving_Algorithms/apple_and_orange.rb
@@ -1,6 +1,21 @@
-def countApplesAndOranges(s, t, a, b, apples, oranges)
+def countApplesAndOranges(start_house, end_house, apple_tree, orange_tree, apples, oranges)
+    total_apples = 0
+    total_oranges = 0
 
+    apples.each do |apple|
+        apple_distance = apple + apple_tree
+        if apple_distance >= start_house && apple_distance <= end_house
+            total_apples += 1
+        end
+    end
+
+    oranges.each do |orange|
+        orange_distance = orange + orange_tree
+        if orange_distance >= start_house && orange_distance <= end_house
+            total_oranges += 1
+        end
+    end
+
+    puts total_apples, total_oranges
 
 end
-
-countApplesAndOranges()
