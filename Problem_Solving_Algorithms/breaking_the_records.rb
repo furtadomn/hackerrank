509604c076129d04b332a6fae42fe347da7f605d
furtadomn <mnfurtado@yahoo.com.br>
2021-09-04 12:16:10 -0300

    changed variable to max_min

--- Problem_Solving_Algorithms/breaking_the_records.rb
+++ Problem_Solving_Algorithms/breaking_the_records.rb
@@ -1,15 +1,15 @@
 def breakingRecords(scores)
   max_score = scores[0]
   min_score = scores[0]
-  min_max = [0, 0]
+  max_min = [0, 0]
   scores.each do |score|
     if score > max_score
-      min_max[0] += 1
+      max_min[0] += 1
       max_score = score
     elsif score < min_score
-      min_max[1] += 1
+      max_min[1] += 1
       min_score = score
     end
   end
-  min_max
+  max_min
 end
