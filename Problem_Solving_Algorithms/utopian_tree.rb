379d526d33e77364d3bd6e1faa8f4bb5727c528c
furtadomn <mnfurtado@yahoo.com.br>
2022-02-14 15:09:53 -0300

    utopian-tree refactored

--- Problem_Solving_Algorithms/utopian_tree.rb
+++ Problem_Solving_Algorithms/utopian_tree.rb
@@ -1,7 +1,7 @@
 def utopianTree(cycles)
   height = 1
   for cycle in 1..cycles
-    cycle % 2 != 0 ? height = (height * 2) : height = (height + 1)
+    cycle % 2 != 0 ? height *= 2 : height += 1
   end
   height
 end
