28d396241fa23ee45403d65c7a58c635ee47c9df
furtadomn <mnfurtado@yahoo.com.br>
2022-02-19 20:47:55 -0300

    strange_advertising

---
+++ Problem_Solving_Algorithms/strange_advertising.rb
@@ -0,0 +1,14 @@
+def viralAdvertising(total_of_days)
+  array_of_days = (1..total_of_days).to_a
+  shared = 5 #day 1
+  liked = 0
+  cumulative = 0
+
+  array_of_days.each do |day|
+    liked = (shared / 2)
+    cumulative += liked
+    shared = liked * 3
+  end
+
+  cumulative
+end
